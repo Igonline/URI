357352a090eee5fa51fb68182967169d6366fb2c
Igor Guimarães Dias <igonline15@gmail.com>
2015-05-29 11:22:33 -0300

    Rewrote code according to java convention Performed some refactoring

--- average2 - 1006/src/Average2.py
+++ average2 - 1006/src/Average2.py
@@ -1,1 +1,13 @@
 __author__ = 'igor'
+
+aWeight = 2.0
+bWeight = 3.0
+cWeight = 5.0
+
+a = input()
+b = input()
+c = input()
+
+weighted_sum = (aWeight * a) + (bWeight * b) + (cWeight * c)
+media = weighted_sum / (aWeight + bWeight + cWeight)
+print "MEDIA = %.1f" % media
