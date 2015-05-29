4893201d798ab3afbf2a6f9d57c7a86693f5479a
Igor Guimarães Dias <igonline15@gmail.com>
2015-05-29 15:07:17 -0300

    Wrote algorithm steps

---
+++ simpleCalculate - 1010/src/SimpleCalculate.py
@@ -0,0 +1,12 @@
+__author__ = 'igor'
+
+# 1. Get input data for product 1 and 2
+
+# Maybe consider using an array of dictionaries...
+
+# 2. Process the total amount to be paid on product 1 and 2
+
+# Iterate over the array, calculating and updating the global var totalAmount
+
+# 3. Print the result (VALOR A PAGAR): pretty straightforward
+
