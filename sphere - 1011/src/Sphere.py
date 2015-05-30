0055c29fc2117632d48320af765370b52d0ec389
Igor Guimarães Dias <igonline15@gmail.com>
2015-05-30 16:12:34 -0300

    Sphere

---
+++ sphere - 1011/src/Sphere.py
@@ -0,0 +1,8 @@
+__author__ = 'igor'
+
+sphereRadius = float( input() )
+
+# What power functions could be used?
+sphereVol = 4/3.0 * 3.14159 * sphereRadius * sphereRadius * sphereRadius
+
+print "VOLUME = %.3f" % sphereVol
