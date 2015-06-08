646aa4737fd63deb1c52bad37d5a838da7d5674e
Igor Guimaraes Dias <igonline15@gmail.com>
2015-06-08 10:31:20 -0300

    Fixed some old exercises

--- ageInDays - 1020/src/AgeInDays.py
+++ ageInDays - 1020/src/AgeInDays.py
@@ -1,1 +1,29 @@
 __author__ = 'igor'
+
+# Dictionary for format
+timeFields = {'years': 0,
+              'months': 0,
+              'days': 0};
+
+def timeFormat(days, timeFields):
+    timeFields['years'] = days / 365
+    days -= timeFields['years'] * 365
+
+    timeFields['months'] = days / 30
+    days -= timeFields['months'] * 30
+
+    timeFields['days'] = days
+
+    return
+
+#1. Read the value
+days = input()
+
+# 2. Calculate how many years, months and days there are in the input value
+timeFormat(days, timeFields)
+
+# 3. Show the results
+print("%d ano(s)\n"
+      "%d mes(es)\n"
+      "%d dia(s)"
+      % (timeFields['years'], timeFields['months'], timeFields['days']))
--- fuelSpent - 1017/FuelSpent.py
+++ fuelSpent - 1017/FuelSpent.py
@@ -1,1 +1,12 @@
 __author__ = 'igor'
+
+EFFICIENCY = 12
+
+timeSpentInTheTrip = input()
+averageVelocity = input()
+
+distance = timeSpentInTheTrip * averageVelocity
+
+fuelSpent = distance / 12.0
+
+print("%.3f" % fuelSpent)
