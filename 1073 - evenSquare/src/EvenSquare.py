f13f92e2a641f3259d5ec27f53d4ec2ffa2ff275
Igor Guimarães Dias <igonline15@gmail.com>
2015-06-26 10:21:22 -0300

    Added Even Square - 1073 exercise

---
+++ 1073 - evenSquare/src/EvenSquare.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Igor Guimarães Dias <igonline15@gmail.com>'
+
+valueX = input()
+for i in range(1, valueX + 1):
+    if (i % 2 == 0):
+        print("%d^2 = %d" % (i, i**2))
